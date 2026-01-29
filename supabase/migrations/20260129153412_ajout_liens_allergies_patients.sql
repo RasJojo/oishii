9a96c15d25ffb0b3766e4246141297af6a125ae8
Matheus Kops Guedes <matheuskopsguedes@gmail.com>
2026-01-29 17:13:23 +0100

    fix : local supabase changes

--- supabase/migrations/20260129153412_ajout_liens_allergies_patients.sql
+++
@@ -1,7 +0,0 @@
-create type "public"."plat" as enum ('ENTREE', 'PLAT', 'DESSERT');
-
-alter table "public"."patients_allergens" add constraint "patients_allergens_patient_id_fkey" FOREIGN KEY (patient_id) REFERENCES public.patients(id) ON DELETE CASCADE not valid;
-
-alter table "public"."patients_allergens" validate constraint "patients_allergens_patient_id_fkey";
-
-
